99cfedfc8675b4c644fc2f3c34674ded98012793
Romanchik00 <116202502+Romanchik00@users.noreply.github.com>
2022-11-28 15:56:23 +0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,21 @@
+console.log("I love JavaScrypt");
+
+let planetName = prompt("Enter name of planet");
+console.log(planetName);
+let correntUser = prompt("Enter user name");
+console.log(correntUser);
+
+
+let myinputarr = [];
+let size = 5;
+for(let a=0; a<size; a++) 
+{
+myinputarr[a] = prompt('Enter array Element ' + (a+1));
+}
+console.log(myinputarr);
+
+let admin ; 
+let name ;
+name = "Jone";
+admin = name;
+alert(admin);
